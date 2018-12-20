755272b262f90f9139d706638ec0ddc344bed47f
Yash Thakur <thakur.yash514@gmail.com>
2018-12-20 13:44:38 +0530

    Update code for api

--- src/app/components/home.js
+++ src/app/components/home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import moment from "moment";
 import _ from "lodash";
 import {
-  Select, Layout, DatePicker, InputNumber, Slider, Card, Icon, List, Pagination, Rate,
+  Select, Layout, DatePicker, InputNumber, Slider, Card, Icon, List, Rate, Spin, Carousel,
 } from "antd";
 import styles from "./home.less";
 
@@ -11,6 +11,8 @@ const {
 } = Layout;
 const { Meta } = Card;
 const { RangePicker } = DatePicker;
+
+const antIconLoader = <Icon type="loading" style={{ fontSize: 38 }} spin />;
 
 export default class Home extends Component {
   constructor(props) {
@@ -26,26 +28,65 @@ export default class Home extends Component {
       guests: 2,
       lat: 52.514050,
       lng: 13.344720,
+      radius: 10000,
+      priceRange: [
+        10,
+        2000,
+      ],
+      resultsLoading: false,
     };
   }
 
-  componentDidMount() {
-    const url = "";
-    fetch(url).then(res => console.log("Result: ", res)).catch(err => console.log("Error: ", err));
-  }
-
   onChange(value, key) {
-    this.setState({ [key]: value });
+    const {
+      guests,
+      dateRange,
+      lat,
+      lng,
+      radius,
+      priceRange,
+    } = this.state;
+    this.setState({
+      [key]: value,
+    });
+    const params = {
+      guests,
+      lat,
+      lng,
+      dateRange,
+      radius,
+      priceRange,
+    };
+    _.assignIn(params, { [key]: value });
+    this.updateResults(params);
   }
 
   disabledDate = current => current && current < moment().endOf("day");
 
-  handleSearch() {
-    fetch("http://localhost:3030/search?q=test").then(res => res.json()).then(data => this.setState({ data: _.map(data, item => ({ text: item.race_name, value: item.race_name })) }));
-  }
+  // handleSearch() {
+  //   fetch("http://localhost:3030/search?q=test").then(res => res.json()).then(data => this.setState({ data: _.map(data, item => ({ text: item.race_name, value: item.race_name })) }));
+  // }
 
   handleChange(value) {
     this.setState({ value });
+  }
+
+  updateResults(params) {
+    const { resultsLoading } = this.state;
+    if (resultsLoading) return;
+    this.setState({ resultsLoading: true });
+    const url = `https://api.stay22.com/v2/hotelscombined?latitude=${params.lat}&longitude=${params.lng}&language=en&currency=USD&guests=${params.guests}&checkin=${_.first(params.dateRange).unix()}&checkout=${_.last(params.dateRange).unix()}&radius=${params.radius}&minprice=${_.first(params.priceRange)}&maxprice=${_.last(params.priceRange)}`;
+    fetch(url).then(res => res.json()).then((res) => {
+      this.setState({
+        data: res,
+        resultsLoading: false,
+      });
+    }).catch((err) => {
+      console.log(err);
+      this.setState({
+        resultsLoading: false,
+      });
+    });
   }
 
   render() {
@@ -55,7 +96,13 @@ export default class Home extends Component {
       value,
       dateRange,
       guests,
+      resultsLoading,
     } = this.state;
+
+    const marks = {
+      1000: "1000 m",
+      20000: "20000 m",
+    };
 
     return (
       <Layout>
@@ -72,7 +119,7 @@ export default class Home extends Component {
               defaultActiveFirstOption={false}
               showArrow={false}
               filterOption={false}
-              onSearch={() => this.handleSearch()}
+              // onSearch={() => this.handleSearch()}
               onChange={val => this.handleChange(val, "value")}
               notFoundContent={null}
               suffixIcon={<Icon type="search" theme="twoTone" twoToneColor="#52c41a" />}
@@ -111,7 +158,15 @@ export default class Home extends Component {
                   <td colSpan={2}>
                     <div>Price Range:</div>
                     <div>
-                      <Slider range defaultValue={[20, 50]} />
+                      <Slider
+                        min={10}
+                        max={2000}
+                        step={10}
+                        tooltipVisible
+                        range
+                        defaultValue={[10, 2000]}
+                        onAfterChange={val => this.onChange(val, "priceRange")}
+                      />
                     </div>
                   </td>
                 </tr>
@@ -119,7 +174,15 @@ export default class Home extends Component {
                   <td colSpan={2}>
                     <div>Distance to Start:</div>
                     <div>
-                      <Slider defaultValue={20} />
+                      <Slider
+                        marks={marks}
+                        min={1000}
+                        max={20000}
+                        step={500}
+                        tooltipVisible
+                        defaultValue={10000}
+                        onAfterChange={val => this.onChange(val, "radius")}
+                      />
                     </div>
                   </td>
                 </tr>
@@ -127,48 +190,59 @@ export default class Home extends Component {
             </table>
           </Sider>
           <Content>
-            <div>
-              <List
-                grid={{
-                  gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl: 3,
-                }}
-                className={styles.results}
-                dataSource={_.get(data, "results", [])}
-                pagination={{ hideOnSinglePage: true, pageSize: 12 }}
-                renderItem={(item) => {
-                  let rating = _.toNumber(((item.rating) / 2).toFixed(1));
-                  if (rating % parseInt(rating, 10) > 0.5) {
-                    rating = parseInt(rating, 10) + 0.5;
-                  } else {
-                    rating = parseInt(rating, 10);
-                  }
-
-                  return (
-                    <List.Item>
-                      <a href={item.link} target="_blank" rel="noreferrer noopener nofollow">
+            {
+              (resultsLoading)
+              && (
+                <div className={styles.loader}>
+                  <Spin size="large" tip="Loading..." indicator={antIconLoader} />
+                </div>
+              )
+            }
+            {
+              (!resultsLoading)
+              && (
+                <div>
+                  <List
+                    grid={{
+                      gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl: 3,
+                    }}
+                    className={styles.results}
+                    dataSource={_.get(data, "results", [])}
+                    pagination={{ hideOnSinglePage: true, pageSize: 12, showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} items` }}
+                    renderItem={item => (
+                      <List.Item>
                         <Card
                           hoverable
-                          cover={<img alt="example" src={_.first(item.thumbnails)} style={{ height: "200px" }} />}
+                          cover={
+                            (
+                              <Carousel dots draggable className={styles.slide}>
+                                {
+                                  _.map(_.get(item, "thumbnails", []), (img, key) => (<img alt="hotel_img" key={key} src={img} style={{ height: "200px" }} />))
+                                }
+                              </Carousel>)
+                          }
                         >
-                          <Meta
-                            title={item.name}
-                          />
-                          <Rate value={item.num_stars} />
-                          Hotel
-                          <br />
-                          Rating:&nbsp;
-                          { _.get(item, "rating", 0) }
-                          /10
-                          <br />
-                          Reviews:&nbsp;
-                          { _.get(item, "review_count", 0) }
+                          <a href={item.link} target="_blank" rel="noreferrer noopener nofollow">
+                            <Meta
+                              title={item.name}
+                            />
+                            <Rate value={item.num_stars} />
+                            Hotel
+                            <br />
+                            Rating:&nbsp;
+                            { _.get(item, "rating", 0) }
+                            /10
+                            <br />
+                            Reviews:&nbsp;
+                            { _.get(item, "review_count", 0) }
+                          </a>
                         </Card>
-                      </a>
-                    </List.Item>
-                  );
-                }}
-              />
-            </div>
+                      </List.Item>
+                    )}
+                  />
+                </div>
+              )
+            }
           </Content>
         </Layout>
       </Layout>
